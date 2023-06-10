c027abd59f7c7f345e4917842d1e94f638af7363
umaremlyn <umaremlyn04@gmail.com>
2023-06-10 15:28:03 +0100

    dghgmh

--- pms/settings.py
+++ pms/settings.py
@@ -91,19 +91,19 @@ AUTH_USER_MODEL = 'user.User'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(        
-        default='postgresql://proma:RsCRrHUYALLEjC3Qlg6KDuDob6OeVtpA@dpg-ci25kg3hp8u1a18ojs80-a:5432/pms_zf1s',
-        conn_max_age=600    
-        )}
-    # "default": {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'pms',
-    #     'USER':'proma',
-    #     'PASSWORD':'RsCRrHUYALLEjC3Qlg6KDuDob6OeVtpA',
-    #     'HOST':'dpg-ci25kg3hp8u1a18ojs80-a',
-    #     'PORT':'5432',
-    # }
-
+    # "default": dj_database_url.config(        
+    #     default='postgresql://proma:RsCRrHUYALLEjC3Qlg6KDuDob6OeVtpA@dpg-ci25kg3hp8u1a18ojs80-a:5432/pms_zf1s',
+    #     conn_max_age=600    
+    #     )}
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pms',
+        'USER':'proma',
+        'PASSWORD':'RsCRrHUYALLEjC3Qlg6KDuDob6OeVtpA',
+        'HOST':'dpg-ci25kg3hp8u1a18ojs80-a',
+        'PORT':'5432',
+    }
+}
 
 # DATABASES = {
 #     'default': dj_database_url.config(        
